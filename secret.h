b8ff7c1c37d03ca1467e0ee01d26416142df3ba2
hasnaazharf <hasnaazhar10@gmail.com>
2020-07-17 00:15:02 +0700

    First Commit

---
+++ secret.h
@@ -0,0 +1,6 @@
+/* WiFi attribute */
+   #define SECRET_SSID "Dedi"
+   #define SECRET_PASS "herdiyana"
+
+   #define FIREBASE_HOST "hasna-smarthome.firebaseio.com"
+   #define FIREBASE_AUTH "cLJ4s5FUhMb3mOYtzd2Hgh106Y6NNePcGxHLDshk"
